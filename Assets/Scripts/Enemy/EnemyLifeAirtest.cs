7ef8bd43219ac17d437084d858a9a1df2f4e5898
CorentinBERLIOZ <berlioz.corentin@gmail.com>
2025-11-27 14:15:13 +0100

    Ajout script life au enemi + pb avec enemy volant
    les ennemy volants ne prennent pas les dégâts le collider ne marche pas

---
+++ Assets/Scripts/Enemy/EnemyLifeAirtest.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyLifeAirtest : MonoBehaviour
+{
+    
+    
+    [SerializeField] private float EnemyTotalLife = 5f;
+    public GameObject DyingEffect;
+    public string BulletTag = "Bullet";
+
+
+    void Start()
+    {
+        
+
+    }
+
+
+    private void Update()
+    {
+        if (EnemyTotalLife <= 0)
+        {
+            EnemyDied();
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == BulletTag)
+        {
+            EnemyTotalLife = EnemyTotalLife - 1f;
+            Debug.Log(EnemyTotalLife);
+
+        }
+    }
+
+    void EnemyDied()
+    {
+        GameObject EffectINS = (GameObject)Instantiate(DyingEffect, transform.position, transform.rotation);
+        Destroy(EffectINS, 2f);
+
+        Destroy(gameObject);
+    }
+}
