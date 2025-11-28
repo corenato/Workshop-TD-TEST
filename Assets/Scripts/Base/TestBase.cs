2bc74bd80677e9af83504250b2b67d673c851ac6
CorentinBERLIOZ <berlioz.corentin@gmail.com>
2025-11-28 15:24:14 +0100

    Mise en place du LD + Base qui détruit les enemis
    faut faire attention pcq les ennemis sans la base retourne à un point ramdom et se suicide

---
+++ Assets/Scripts/Base/TestBase.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TestBase : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OUCH");
+        Destroy(other.gameObject);
+    }
+}
