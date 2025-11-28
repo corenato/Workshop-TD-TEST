2c54ad3c5306c95ca07dd6db3bdddc7676e2e9f4
CorentinBERLIOZ <berlioz.corentin@gmail.com>
2025-11-28 12:35:29 +0100

    Rework Wave un vague sur plusieur chemin

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -2,81 +2,77 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Jobs;
 using UnityEngine.UIElements;
-
 public class EnemySpawner : MonoBehaviour
 {
     public Wave[] Waves;
-    [SerializeField] private Transform Spawnpoint;
+    [SerializeField] private Transform[] SpawnPoints;
     [SerializeField] private float TimeBetweenEnemies = 1f;
     [SerializeField] private float TimeBetweenWave = 2f;
-    [SerializeField] private float EnemyCountDown = 2f;
+    //[SerializeField] private float EnemyCountDown = 2f;
     [SerializeField] private float CountDown = 2f;
-    public static int SpawnedEnemyCount = 0;       
+    [SerializeField] private float MinRange = 0f;
+    [SerializeField] private float MaxRange = 5f;
+    public static int SpawnedEnemyCount = 0;
     public WayPoints PathToUse;
     private int WaveIndex = 0;
-
     public int StartEnemyNumber = 0;
 
+    void Awake()
+    {
+
+    }
     public void Start()
     {
         Debug.Log(WaveIndex);
     }
-
-
     void Update()
     {
-        
-            
         if (WaveIndex >= Waves.Length)
         {
             Debug.Log("bravo terminée");
             return;
         }
-
         if (SpawnedEnemyCount > 0)
         {
             return;
         }
-
-        if(CountDown <= 0)
+        if (CountDown <= 0)
         {
             StartCoroutine(DoSpawn());
             CountDown = TimeBetweenWave;
-            
             return;
         }
-        
         CountDown -= Time.deltaTime;
     }
-
     IEnumerator DoSpawn()
     {
         Wave wave = Waves[WaveIndex];
-
         foreach (var enemyEntry in wave.Enemies)
         {
-            for (int i = 0; i < enemyEntry.Count; i++)
-            {
-
-                SpawnEnemy(enemyEntry.Enemy);
-                TimeBetweenEnemies = Random.Range(0f, 5f);
-
-                yield return new WaitForSeconds(TimeBetweenEnemies / enemyEntry.rate);
+            for (int i = 0; i < enemyEntry.Count; i++) 
+            { 
+                SpawnEnemy(enemyEntry.Enemy); 
+                TimeBetweenEnemies = Random.Range(MinRange, MaxRange);
+                yield return new WaitForSeconds(TimeBetweenEnemies / enemyEntry.rate); 
             }
         }
 
+        
         WaveIndex++;
-
-        
     }
+    void SpawnEnemy(GameObject enemy) 
+    { 
+        Transform randomSpawn = SpawnPoints[Random.Range(0,SpawnPoints.Length)];  
+        GameObject instance = Instantiate(enemy, randomSpawn.position, randomSpawn.rotation);
 
 
-    void SpawnEnemy(GameObject enemy)
-    {
-            GameObject instance = Instantiate(enemy, Spawnpoint.position, Spawnpoint.rotation);
-            instance.GetComponent<EnemyMovement>().Path = PathToUse;
-        
-            SpawnedEnemyCount++;
+        WayPoints wp = randomSpawn.GetComponent<WayPoints>();
+        instance.GetComponent<EnemyMovement>().Path = wp;
+
+
+        SpawnedEnemyCount++; 
     }
-    
 }
+
+
+
